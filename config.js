30c9d941f0c8cbb3cb46895a13f6309c1ced8f05
TonyQ <tonylovejava@gmail.com>
2010-12-24 23:38:53 +0800

    move out the config

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * The config for sudoku
+ */
+
+ch_client = "laurynas2009";
+ch_width = 400;
+ch_height = 90;
+ch_type = "mpu";
+ch_sid = "Chitika Default";
+ch_color_site_link = "#0000CC";
+ch_color_title = "#0000CC";
+ch_color_border = "#FFFFFF";
+ch_color_text = "#000000";
+ch_color_bg = "#FFFFFF";
