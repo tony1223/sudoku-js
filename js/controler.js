0bf47aa1de844d382305057c12d111ee816ec61d
TonyQ <tonylovejava@gmail.com>
2010-12-25 16:51:46 +0800

    move out widget from controller  and add test case

--- js/controler.js
+++ js/controler.js
@@ -1,157 +1,17 @@
 /**
  * The controller for main.html
+ * We mean a controller
  */
-
-/**
- * init for sudoku box
- */
-function SudokuBox(context){
-
-	//add every number helper (1-9) each cell here.
-	for(var i=0;i<9;i++){
-		for(var j=0;j<9;j++){
-			var c = i;
-			var r = j;
-			var g = Math.floor(i/3)*3 + Math.floor(j/3);
-			var one=$('<div class="in c'+c+' r'+r+'" r="'+r+'" c="'+c+'" g="'+g+'"></div>').append('<span></span>').appendTo(context);
-			for(var m=1;m<=9;m++){
-				var t=3+Math.floor((m-1)/3)*12;
-				var l=3+Math.floor((m-1)%3)*12;
-				$('<div class="mm" m="'+m+'">'+m+'</div>').appendTo(one).css({top:t,left:l});
-			}
-		}
-	}
-}
-
-/**
- * bind cell event after cell is created
- * @return SudukuBox
- */
-SudokuBox.prototype.bindCell=function(){
-	var sudokubox = this;
-	var $in=this.getCells();
-	$in.click(function(){
-		$tar=$(this);
-		sudokubox.startUserInput($(this));
-	});
-	return this;
-}
-
-/**
- * focus all the related cells ,
- * including the horizontal line , the vertical line and the groups.
- * @param r horizontal index  from 0 ~ 8
- * @param c vertical index from 0 ~ 8
- * @param g group , 3*3 cells for a group , it's in order from left to right ,
- *   <pre> 0,1,2,
- *          3,4,5,
- *          6,7,8 </pre>
- * @return SudukuBox
- */
-SudokuBox.prototype.focusCells=function($tar){
-	var $in=this.getCells();
-	$in.removeClass('fo');
-
-	this.getRelativeCell($tar).addClass('fo');
-	return this;
-}
-
-/**
- * update cells for remove the user entered number,
- * if user eneter "1" , all the cell for the horizontal line ,
- * the vertical line and the groups should not exist "1".
- * @param activeCell , we use the cell's value to update cells.
- * @return
- *
- * @Todo here's a bug if user modify the number twice , it will be incorrect.
- */
-SudokuBox.prototype.updateCells=function(activeCell){
-	var n=activeCell.find('span').html();
-	//remove all the numbers in cell
-	activeCell.find('.mm').remove();
-	this.getRelativeCell(activeCell).find('.mm[m="'+n+'"]').remove();
-
-}
-SudokuBox.prototype.getRelativeCell=function(activeCell){
-	var r = activeCell.attr('r'),
-		c = activeCell.attr('c'),
-		g = activeCell.attr('g');
-
-	return this.getCells().filter('[r="'+r+'"],[c="'+c+'"],[g="'+g+'"]');
-}
-/**
- * get all the cell contrains
- * @return
- */
-SudokuBox.prototype.getCells=function(){
-	return $('.in');
-}
-
-/**
- * we need to hide numbers when user is tpying his number.
- * This implement is too low-end , that any user should not call this function directly.
- * @param activeCell
- * @return
- */
-/* private */ SudokuBox.prototype._maskCell=function(activeCell){
-	/**
-	 * we use a div to overlap the cells numbers , and we call it "put"
-	 */
-
-	var txt = activeCell.css('margin-top');
-
-	//use parseInt(xxx , 10)  instead parseInt(xxx) to prevent Octal number issue.
-	var t = activeCell.position().top + parseInt( txt.substr(0,txt.length) , 10);
-	var l = activeCell.position().left;
-	var $put = $('.put');
-	$put.css({left:l,top:t}).show();
-}
-
-/**
- *
- * @param $tar
- * @return
- * @Todo a warning for user entered a already exist number.
- */
-SudokuBox.prototype.startUserInput=function($tar){
-	var r=$tar.attr('r'),
-		c=$tar.attr('c'),
-	    $in=this.getCells(),
-	    $put=$('.put'),
-	    sudokubox = this;
-	this._maskCell($tar);
-	this.focusCells($tar);
-	$('<input type="text" r="'+r+'" c+"'+c+'" />').appendTo($put).keyup(function(){
-		sudokubox.input(r, c, $(this).val());
-		$in.removeClass('fo');
-		$put.hide().find('input').remove();
-	}).focusout(function(){
-		$in.removeClass('fo');
-		$put.hide().find('input').remove();
-	}).width(20).height(20).focus();
-}
-
-/**
- * return selected cell as jQuery context.
- */
-SudokuBox.prototype.findCell=function(r,c){
-	return this.getCells().filter('[r="'+r+'"][c="'+c+'"]');
-}
-/**
- * for input a word
- */
-SudokuBox.prototype.input=function(r,c,value){
-	var activeCell = this.findCell(r,c);
-	activeCell.find('span').html(value);
-	sudokubox.updateCells(activeCell);
-}
-
 
 var box=$('.box');
 
-var sudokubox = new SudokuBox(box);
+var sudokubox = new SudokuBox(box,{
+	//a log for easy to build a test case
+	afterInput:function(r,c,value){
+		$("#gen-result").val($("#gen-result").val()+"\nsudokubox.input("+r+", "+c+", "+value+");");
+	}
+});
 sudokubox.bindCell();
-
 
 
 /**
---
+++ test/testCase1.js
@@ -0,0 +1,46 @@
+
+sudokubox.input(0, 0, 4);
+sudokubox.input(0, 2, 8);
+sudokubox.input(0, 4, 9);
+sudokubox.input(0, 5, 2);
+
+sudokubox.input(1, 6, 8);
+sudokubox.input(1, 8, 4);
+
+sudokubox.input(2, 2, 1);
+sudokubox.input(2, 3, 4);
+sudokubox.input(2, 4, 6);
+sudokubox.input(2, 5, 8);
+sudokubox.input(2, 6, 7);
+sudokubox.input(2, 7, 9);
+
+sudokubox.input(3, 0, 8);
+sudokubox.input(3, 1, 1);
+sudokubox.input(3, 4, 3);
+sudokubox.input(3, 6, 5);
+
+sudokubox.input(4, 0, 9);
+sudokubox.input(4, 3, 8);
+sudokubox.input(4, 8, 6);
+
+sudokubox.input(5, 2, 2);
+sudokubox.input(5, 4, 4);
+sudokubox.input(5, 7, 8);
+
+
+sudokubox.input(6, 0, 1);
+sudokubox.input(6, 1, 3);
+sudokubox.input(6, 2, 5);
+sudokubox.input(6, 4, 7);
+sudokubox.input(6, 5, 4);
+sudokubox.input(6, 6, 2);
+sudokubox.input(6, 8, 8);
+
+sudokubox.input(7, 0, 6);
+sudokubox.input(7, 1, 8);
+sudokubox.input(7, 2, 7);
+
+sudokubox.input(8, 3, 1);
+sudokubox.input(8, 4, 8);
+sudokubox.input(8, 6, 3);
+sudokubox.input(8, 8, 7);
