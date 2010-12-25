1af7cacf4ea24e2fba30342db39a7cd6380c7f30
TonyQ <tonylovejava@gmail.com>
2010-12-26 01:08:37 +0800

    format widget instance function

--- js/sudokuWidget.js
+++ js/sudokuWidget.js
@@ -35,146 +35,144 @@ function SudokuBox(context, opts) {
 	this.bindCell();
 }
 
-/**
- * bind cell event after cell is created
- *
- * @return SudukuBox
- */
-SudokuBox.prototype.bindCell = function() {
-	var sudokubox = this;
-	var $in = this.getCells();
-	$in.click(function() {
-		$tar = $(this);
-		sudokubox.startUserInput($(this));
-	});
-	return this;
-}
-
-/**
- * focus all the related cells , including the horizontal line , the vertical
- * line and the groups.
- *
- * @param r
- *            horizontal index from 0 ~ 8
- * @param c
- *            vertical index from 0 ~ 8
- * @param g
- *            group , 3*3 cells for a group , it's in order from left to right ,
- *
- * <pre>
- * 0, 1, 2, 3, 4, 5, 6, 7, 8
- * </pre>
- *
- * @return SudukuBox
- */
-SudokuBox.prototype.focusCells = function($tar) {
-	var $in = this.getCells();
-	$in.removeClass('fo');
-
-	this.getRelativeCell($tar).addClass('fo');
-	return this;
-}
-
-/**
- * update cells for remove the user entered number, if user eneter "1" , all the
- * cell for the horizontal line , the vertical line and the groups should not
- * exist "1".
- *
- * @param activeCell ,
- *            we use the cell's value to update cells.
- * @return
- *
- * @Todo here's a bug if user modify the number twice , it will be incorrect.
- */
-SudokuBox.prototype.updateCells = function(activeCell) {
-	var n = activeCell.find('span').html();
-	// remove all the numbers in cell
-	activeCell.find('.mm').remove();
-	this.getRelativeCell(activeCell).find('.mm[m="' + n + '"]').remove();
-
-}
-SudokuBox.prototype.getRelativeCell = function(activeCell) {
-	var r = activeCell.attr('r'), c = activeCell.attr('c'), g = activeCell
-			.attr('g');
-
-	return this.getCells().filter(
-			'[r="' + r + '"],[c="' + c + '"],[g="' + g + '"]');
-}
-/**
- * get all the cell contrains
- *
- * @return
- */
-SudokuBox.prototype.getCells = function() {
-	return $('.in');
-}
-
-/**
- * we need to hide numbers when user is tpying his number. This implement is too
- * low-end , that any user should not call this function directly.
- *
- * @param activeCell
- * @return
- */
-/* private */SudokuBox.prototype._maskCell = function(activeCell) {
+SudokuBox.prototype = {
 	/**
-	 * we use a div to overlap the cells numbers , and we call it "put"
+	 * bind cell event after cell is created
+	 *
+	 * @return SudukuBox
 	 */
+	bindCell : function() {
+		var sudokubox = this;
+		var $in = this.getCells();
+		$in.click(function() {
+			$tar = $(this);
+			sudokubox.startUserInput($(this));
+		});
+		return this;
+	},
+	/**
+	 * focus all the related cells , including the horizontal line , the vertical
+	 * line and the groups.
+	 *
+	 * @param r
+	 *            horizontal index from 0 ~ 8
+	 * @param c
+	 *            vertical index from 0 ~ 8
+	 * @param g
+	 *            group , 3*3 cells for a group , it's in order from left to right ,
+	 *
+	 * <pre>
+	 * 0, 1, 2, 3, 4, 5, 6, 7, 8
+	 * </pre>
+	 *
+	 * @return SudukuBox
+	 */
+	focusCells : function($tar) {
+		var $in = this.getCells();
+		$in.removeClass('fo');
 
-	var txt = activeCell.css('margin-top');
+		this.getRelativeCell($tar).addClass('fo');
+		return this;
+	},
+	/**
+	 * update cells for remove the user entered number, if user eneter "1" , all the
+	 * cell for the horizontal line , the vertical line and the groups should not
+	 * exist "1".
+	 *
+	 * @param activeCell ,
+	 *            we use the cell's value to update cells.
+	 * @return
+	 *
+	 * @Todo here's a bug if user modify the number twice , it will be incorrect.
+	 */
+	updateCells : function(activeCell) {
+		var n = activeCell.find('span').html();
+		// remove all the numbers in cell
+		activeCell.find('.mm').remove();
+		this.getRelativeCell(activeCell).find('.mm[m="' + n + '"]').remove();
 
-	// use parseInt(xxx , 10) instead parseInt(xxx) to prevent Octal number
-	// issue.
-	var t = activeCell.position().top + parseInt(txt.substr(0, txt.length), 10);
-	var l = activeCell.position().left;
-	var $put = $('.put');
-	$put.css( {
-		left : l,
-		top : t
-	}).show();
-}
+	},
+	getRelativeCell : function(activeCell) {
+		var r = activeCell.attr('r'), c = activeCell.attr('c'), g = activeCell
+				.attr('g');
 
-/**
- *
- * @param $tar
- * @return
- * @Todo a warning for user entered a already exist number.
- */
-SudokuBox.prototype.startUserInput = function($tar) {
-	var r = $tar.attr('r'), c = $tar.attr('c'), $in = this.getCells(), $put = $('.put'), that = this;
-	this._maskCell($tar);
-	this.focusCells($tar);
-	$('<input type="text" r="' + r + '" c+"' + c + '" />').appendTo($put)
-			.keyup(function() {
-				that.input(r, c, $(this).val());
-				$in.removeClass('fo');
-				$put.hide().find('input').remove();
-			}).focusout(function() {
-				$in.removeClass('fo');
-				$put.hide().find('input').remove();
-			}).width(20).height(20).focus();
-}
+		return this.getCells().filter(
+				'[r="' + r + '"],[c="' + c + '"],[g="' + g + '"]');
+	},
 
-/**
- * return selected cell as jQuery context.
- */
-SudokuBox.prototype.findCell = function(r, c) {
-	return this.getCells().filter('[r="' + r + '"][c="' + c + '"]');
-}
-/**
- * for input a word
- */
-SudokuBox.prototype.input = function(r, c, value, solved) {
-	var activeCell = this.findCell(r, c), span = activeCell.find('span');
+	/**
+	 * get all the cell contrains
+	 *
+	 * @return
+	 */
+	getCells : function() {
+		return $('.in');
+	},
+	/**
+	 * we need to hide numbers when user is tpying his number. This implement is too
+	 * low-end , that any user should not call this function directly.
+	 *
+	 * @param activeCell
+	 * @return
+	 */
+	/* private */ _maskCell : function(activeCell) {
+		/**
+		 * we use a div to overlap the cells numbers , and we call it "put"
+		 */
 
-	span.html(value);
-	this.updateCells(activeCell);
+		var txt = activeCell.css('margin-top');
 
-	if (solved)
-		span.addClass('red');
+		// use parseInt(xxx , 10) instead parseInt(xxx) to prevent Octal number
+		// issue.
+		var t = activeCell.position().top + parseInt(txt.substr(0, txt.length), 10);
+		var l = activeCell.position().left;
+		var $put = $('.put');
+		$put.css( {
+			left : l,
+			top : t
+		}).show();
+	},
+	/**
+	 *
+	 * @param $tar
+	 * @return
+	 * @Todo a warning for user entered a already exist number.
+	 */
+	startUserInput : function($tar) {
+		var r = $tar.attr('r'), c = $tar.attr('c'), $in = this.getCells(), $put = $('.put'), that = this;
+		this._maskCell($tar);
+		this.focusCells($tar);
+		$('<input type="text" r="' + r + '" c+"' + c + '" />').appendTo($put)
+				.keyup(function() {
+					that.input(r, c, $(this).val());
+					$in.removeClass('fo');
+					$put.hide().find('input').remove();
+				}).focusout(function() {
+					$in.removeClass('fo');
+					$put.hide().find('input').remove();
+				}).width(20).height(20).focus();
+	},
+	/**
+	 * return selected cell as jQuery context.
+	 */
+	findCell : function(r, c) {
+		return this.getCells().filter('[r="' + r + '"][c="' + c + '"]');
+	},
+	/**
+	 * for input a word
+	 */
+	input : function(r, c, value, solved) {
+		var activeCell = this.findCell(r, c), span = activeCell.find('span');
 
-	if ($.isFunction(this._doAfterInput)) // a callback for user customize
-		// event
-		this._doAfterInput.apply(this, [ r, c, value ]);
+		span.html(value);
+		this.updateCells(activeCell);
 
-}
+		if (solved)
+			span.addClass('red');
+
+		if ($.isFunction(this._doAfterInput)) // a callback for user customize
+			// event
+			this._doAfterInput.apply(this, [ r, c, value ]);
+
+	}
+};
