73a6371c706a605d7dd68a51ad1022bd6243c5a5
TonyQ <tonylovejava@gmail.com>
2010-12-25 16:01:48 +0800

    move out the strategy function and utils

--- js/strategy.js
+++ js/strategy.js
@@ -18,7 +18,7 @@ var SolveStrategy = {
 				for(var j = 1 ; j <= 9 ; j++){
 					var tmp = mm.filter('[m="' + j + '"]');
 					if( tmp.length == 1 ){
-						find_one(tmp);
+						utils.find_one(tmp);
 						flag = false;
 					}
 				}
@@ -29,7 +29,7 @@ var SolveStrategy = {
 		$in.has('.mm').each(function(){
 			var mm=$(this).find('.mm');
 			if(mm.length==1){
-				find_one(mm);
+				utils.find_one(mm);
 				flag = false;
 			}
 		});
@@ -75,10 +75,119 @@ var SolveStrategy = {
 	}
 }
 
-function find_one($this){
-	$this.siblings('span').html($this.attr('m')).addClass('red');
-	$this.siblings('.mm').remove();
-	var p=$this.parent('.in');
-	sudokubox.updateCells(p);
-	$this.remove();
+var utils={
+	/**
+	 * update the cell value with the first mn value and remove it,
+	 * and updated relative cells .
+	 * @param activeCell the target cell
+	 * @return
+	 */
+	find_one:function(activeCell){
+		activeCell.siblings('span').html(activeCell.attr('m')).addClass('red');
+		activeCell.siblings('.mm').remove();
+		var p=activeCell.parent('.in');
+		sudokubox.updateCells(p);
+		activeCell.remove();
+	}
+
+}
+
+
+/**
+ * determine the group.
+ * @return
+ */
+
+function check_4(){
+	var $in = $(".in");
+	var flag = true;
+	$.each(['r','c','g'],function(k,v){
+		for(var i=0;i<9;i++){
+			var tmp = $in.filter('['+v+'="'+i+'"]').has('.mm');
+			if(!check_4_1(tmp)){
+				flag = false;
+			}
+			if(!check_4_2(tmp)){
+				flag = false;
+			}
+		}
+	});
+	if(flag){
+		check_5();
+	}else{
+		solve();
+	}
+}
+function check_5(){
+	var $in = $(".in");
+	var flag = true;
+	var type=['r','c'];
+	$.each(['r','c','g'],function(k,v){
+		for(var a=0;a<8;a++){
+			for(var m=1;m<=9;m++){
+				var mm = $in.filter('['+v+'="'+a+'"]').find('.mm[m="'+m+'"]');
+				if(mm.length != 2)continue;
+				for(var b=a+1;b<9;b++){
+					var nn = $in.filter('['+v+'="'+b+'"]').find('.mm[m="'+m+'"]');
+					if(nn.length != 2)continue;
+					var g=(v=='r')?'c':'r';
+					var c = mm.eq(0).parent().attr(g);
+					var d = mm.eq(1).parent().attr(g);
+					if(nn.eq(0).parent().attr(g) ==c && nn.eq(1).parent().attr(g) == d){
+						var target = $in.filter('['+g+'="'+c+'"],['+g+'="'+d+'"]').find('.mm[m="'+m+'"]').not(mm).not(nn);
+						if(target.length > 0){
+							flag=false;
+							target.remove();
+						}
+					}
+				}
+			}
+		}
+	});
+	if(flag){
+		$('.info').html('結束');
+	}else{
+		solve();
+	}
+}
+function check_4_1(tmp){
+	var flag = true;
+	for(var m=1;m<9;m++){
+		var mm = tmp.find('.mm[m="'+m+'"]');
+		if(mm.length != 2)continue
+		for(var n=m+1;n<=9;n++){
+			var nn = tmp.find('.mm[m="'+n+'"]');
+			if(nn.length != 2)continue
+			if(mm.eq(0).parent().get(0) == nn.eq(0).parent().get(0) && mm.eq(1).parent().get(0) == nn.eq(1).parent().get(0)){
+				var target = mm.parent().find('.mm').not(nn).not(mm);
+				if(target.length > 0){
+					target.remove();
+					flag=false;
+				}
+			}
+		}
+	}
+	return flag;
+}
+function check_4_2(tmp){
+	var flag = true;
+	var tmp_length = tmp.length;
+	for(var m=0;m<tmp_length-1;m++){
+		var mm = tmp.eq(m).find('.mm');
+		if(mm.length != 2)continue;
+		for(n=m+1;n<tmp_length;n++){
+			var nn = tmp.eq(n).find('.mm');
+			if(nn.length != 2)continue;
+			var a = mm.eq(0).attr('m');
+			var b = mm.eq(1).attr('m');
+			if(nn.eq(0).attr('m') == a && nn.eq(1).attr('m') == b){
+				var target = tmp.find('.mm').filter('[m="'+a+'"],[m="'+b+'"]').not(mm).not(nn);
+				if(target.length > 0){
+					target.remove();
+					flag=false;
+				}
+			}
+		}
+	}
+	return flag;
 }
