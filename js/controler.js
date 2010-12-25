df2c9d18fdd6c43a8cb2da499fbc09fef57b6b18
TonyQ <tonylovejava@gmail.com>
2010-12-25 15:46:51 +0800

    move out the check_3 to strategy

--- js/controler.js
+++ js/controler.js
@@ -144,7 +144,7 @@ sudokubox.bindCell();
  */
 var SolveStrategy = {
 
-	one:function(sudoku){
+	one:function(sudoku){ //original check_1
 		var flag = true,
 			$in=sudoku.getCells();
 		$('span').removeClass('red');
@@ -176,67 +176,52 @@ var SolveStrategy = {
 		});
 
 		return flag;
+	},
+	group:function(sudoku){ //original check_3
+		var flag = true,
+		    $in=sudoku.getCells();
+		// iterate the 9 groups .
+		for(var g = 0 ; g < 9 ; g++){
+			var mm = $in.filter('[g="' + g + '"]').find('.mm');
+
+			//if the group is already solved , just skip it
+			if( mm.length == 0) continue;
+
+			for(var m = 1 ; m <= 9 ; m++){
+				var tmp = mm.filter('[m="' + m + '"]');
+				var tmp_length = tmp.length;
+				if(tmp_length == 0) continue
+				var p = tmp.parent();
+				var r = p.eq(0).attr('r');
+				var c = p.eq(0).attr('c');
+				var t_r = p.filter('[r="'+r+'"]');
+				var t_c = p.filter('[c="'+c+'"]');
+				if(t_r.length == p.length){
+					var target = $in.filter('[r="'+r+'"]').not(p).find('.mm[m="'+m+'"]');
+					if(target.length > 0){
+						flag = false;
+						target.remove();
+					}
+				}
+				if(t_c.length == p.length){
+					var target = $in.filter('[c="'+c+'"]').not(p).find('.mm[m="'+m+'"]');
+					if(target.length > 0){
+						flag = false;
+						target.remove();
+					}
+				}
+			}
+		}
+		return flag;
 	}
 }
+
 
 /**
- * Start to resolve the soduku. (The event handler for the button)
+ * determine the group.
  * @return
  */
-function solve(){
-	var flag = SolveStrategy.one(sudokubox);
-	if(flag){ //if nothing changes
-		//if there not any existing empty cell ,
-		//it means game is over now.
-		if($('.mm').length == 0){
-			$('.info').html('結束');
-			return;
-		}
-		check_3();
 
-	}else{
-		// if it change something , we just run it again after.
-		// because if you fill a cell , it will effect other cell's number(mn).
-		setTimeout(solve,500);
-	}
-}
-function check_3(){
-	var $in = $(".in");
-	var flag = true;
-	for(var g=0;g<9;g++){
-		var mm = $in.filter('[g="'+g+'"]').find('.mm');
-		if(mm.length == 0)continue;
-		for(var m=1;m<=9;m++){
-			var tmp = mm.filter('[m="'+m+'"]');
-			var tmp_length = tmp.length;
-			if(tmp_length == 0)continue
-			var p = tmp.parent();
-			var r = p.eq(0).attr('r');
-			var c = p.eq(0).attr('c');
-			var t_r = p.filter('[r="'+r+'"]');
-			var t_c = p.filter('[c="'+c+'"]');
-			if(t_r.length == p.length){
-				var target = $in.filter('[r="'+r+'"]').not(p).find('.mm[m="'+m+'"]');
-				if(target.length > 0){
-					flag = false;
-					target.remove();
-				}
-			}
-			if(t_c.length == p.length){
-				var target = $in.filter('[c="'+c+'"]').not(p).find('.mm[m="'+m+'"]');
-				if(target.length > 0){
-					flag = false;
-					target.remove();
-				}
-			}
-		}
-	}
-	if(flag){
-		check_4();
-	}else{
-		solve();
-	}
-}
 function check_4(){
 	var $in = $(".in");
 	var flag = true;
@@ -336,5 +321,32 @@ function find_one($this){
 	var p=$this.parent('.in');
 	sudokubox.updateCells(p);
 	$this.remove();
+}
+
+/**
+ * Start to resolve the soduku. (The event handler for the button)
+ * @return
+ */
+function solve(){
+	var flag = SolveStrategy.one(sudokubox);
+	if(SolveStrategy.one(sudokubox)){ //if nothing changes
+		//if there not any existing empty cell ,
+		//it means game is over now.
+		if($('.mm').length == 0){
+			$('.info').html('結束');
+			return;
+		}
+
+		if(SolveStrategy.group(sudokubox)){
+			check_4();
+		}else{
+			solve();
+		}
+
+	}else{
+		// if it change something , we just run it again after.
+		// because if you fill a cell , it will effect other cell's number(mn).
+		setTimeout(solve,500);
+	}
 }
 $('#check_ok2').click(solve);
